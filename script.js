799f2ee37ea036e4304b7fdbebb73916a5e84fd9
Connor Savoy <connorsavoy812@gmail.com>
2023-01-26 16:38:44 -0500

    added in my javascript function, using dayjs, I'm able to get the current time and compare that time with the time-blocks to change their color, as well as added in a feature to save the information in your local storage.

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  // Gets Current Date and applies it to current day parapraph in header
+
+  let currentDay = dayjs().format('dddd / MMMM / YYYY');
+  $('#currentDay').text(currentDay);
+
+  // Gets current time in the form of hours
+
+  let currentTime = dayjs().format('H');
+
+  // Loops through every div with the class .time-block and gets Id's, and based on the id will
+  // change the color of the box 
+
+  $('.time-block').each(function () {
+
+    timeBlock = parseInt(this.id);
+    console.log(timeBlock);
+
+    if (timeBlock < currentTime) {
+      $(this).addClass('past');
+    } else if (timeBlock == currentTime) {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    } else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
+  })
+
+  // Event listener for save button and adds .description to localStorage
+
+  $(".saveBtn").on("click", function () {
+
+    let text = $(this).siblings('.description').val();
+    let time = $(this).parent().attr('id');
+    localStorage.setItem('Hour-' + time, text);
+  })
+
+  $("#9 .description").val(localStorage.getItem("Hour-9"));
+  $("#10 .description").val(localStorage.getItem("Hour-10"));
+  $("#11 .description").val(localStorage.getItem("Hour-11"));
+  $("#12 .description").val(localStorage.getItem("Hour-12"));
+  $("#13 .description").val(localStorage.getItem("Hour-13"));
+  $("#14 .description").val(localStorage.getItem("Hour-14"));
+  $("#15 .description").val(localStorage.getItem("Hour-15"));
+  $("#16 .description").val(localStorage.getItem("Hour-16"));
+  $("#17 .description").val(localStorage.getItem("Hour-17"));
+
+  // Reloads the page every minute so in the hour changes, the boxes update color
+
+  setTimeout(function () {
+    window.location.reload();
+  }, 600000);
+
 });
